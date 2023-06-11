93655c15077459734e4c627f1295d4ed5adb05c7
Felipe8959 <felipe.santos369@hotmail.com>
2023-06-11 18:31:13 -0300

    site_iten

---
+++ static/script_index.js
@@ -0,0 +1,48 @@
+var txt1 = 'Confiança e credibilidade';
+var txt2 = 'O ITEN – Instituto Tecnológico de Ensaios Ltda. – é um laboratório nacional de ensaios independentes que realiza os mais variados tipos de serviços e atua nas áreas elétrica, magnética, eletrônica, física, mecânica, óptica, térmica, acústica e química.';
+var txt3 = 'Contamos com uma equipe de profissionais altamente qualificada e possuímos uma infraestrutura completa com diversas salas especiais para análises e ensaios e mais cinco salas para suporte e administração. Além disso, o principal objetivo da nossa empresa é fortalecer e proporcionar credibilidade aos produtos e serviços para os mais distintos segmentos do mercado.'
+var speed = 20;
+
+function typeWriter1() {
+    var i = 0;
+    function addChar() {
+        if (i < txt1.length) {
+            document.getElementById("welcome").innerHTML += txt1.charAt(i);
+            i++;
+            setTimeout(addChar, speed);
+        } else {
+            typeWriter2();
+        }
+    }
+    addChar();
+}
+
+function typeWriter2() {
+    var i = 0;
+    function addChar() {
+        if (i < txt2.length) {
+            document.getElementById("message").innerHTML += txt2.charAt(i);
+            i++;
+            setTimeout(addChar, speed);
+        } else {
+            typeWriter3();
+        }
+    }
+    addChar();
+}
+
+function typeWriter3() {
+    var i = 0;
+    function addChar() {
+        if (i < txt3.length) {
+            document.getElementById("message2").innerHTML += txt3.charAt(i);
+            i++;
+            setTimeout(addChar, speed);
+        }
+    }
+    addChar();
+}
+
+window.onload = function() {
+    typeWriter1();
+};
---
+++ static/script_sidebar.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {
+    var navbar = document.getElementById('sidebar');
+    if (window.pageYOffset > 0) {
+        navbar.classList.remove('bg-body-tertiary');
+        navbar.classList.add('bg-body-tertiary-opaque');
+    } else {
+        navbar.classList.remove('bg-body-tertiary-opaque');
+        navbar.classList.add('bg-body-tertiary');
+    }
+};
